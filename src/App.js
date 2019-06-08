90ace115328fb56bc5b90c855102bec3a2e200dd
anza19 <anza.k.9539@gmail.com>
2019-06-08 02:01:24 -0400

    Fixed issue with CSSTransitionGroup not working. Took me f**king ages. -_- Btwm transition STILL NOT WORKING, IDIOT!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import someImage from './mama_papa.jpg';
 import './App.css';
+import {CSSTransitionGroup} from 'react-transition-group';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>WORLD's GREATEST PARENTS! :D</h1>
-        <img src={someImage} className="image-resize"/>
+        <CSSTransitionGroup 
+          in={false}
+          appear={true}
+          timeout={1000}
+          classNames="fade"          
+          >
+          <img src={someImage} className="image-resize"/>
+        </CSSTransitionGroup>
       </header>
     </div>
   );
