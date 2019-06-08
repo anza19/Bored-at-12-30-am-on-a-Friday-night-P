6caac7bc953bee2d188047a49b71693826569232
anza19 <anza.k.9539@gmail.com>
2019-06-08 12:50:29 -0400

    This boilerplate allows me to change stuff with the click button! HAHAHAAHAHAH! xD

--- src/App.js
+++ src/App.js
@@ -3,22 +3,49 @@ import someImage from './mama_papa.jpg';
 import './App.css';
 import {CSSTransitionGroup} from 'react-transition-group';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>WORLD's GREATEST PARENTS! :D</h1>
-        <CSSTransitionGroup 
-          in={false}
-          appear={true}
-          timeout={1000}
-          classNames="fade"          
-          >
-          <img src={someImage} className="image-resize"/>
-        </CSSTransitionGroup>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    //define state here
+    this.state = {
+      name: "Initial State!"
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    this.setState({name: "React Rocks"});
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.handleClick}>CLICK ME</button>
+        <h1>{this.state.name}</h1>
+      </div>
+
+    )
+  }
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>WORLD's GREATEST PARENTS! :D</h1>
+//         <CSSTransitionGroup 
+//           in={false}
+//           appear={true}
+//           timeout={1000}
+//           classNames="fade"          
+//           >
+//           <img src={someImage} className="image-resize"/>
+//         </CSSTransitionGroup>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
